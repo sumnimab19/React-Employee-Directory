1e229eefa2579c3141d97de1df9a4a68af710fc5
Sumnima B <sumnimabhandari@gmail.com>
2020-08-14 22:58:37 -0700

    Added search box and updated style.css on navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 // import './App.css';
 import Navbar from "./components/Navbar";
+import SearchBox from "./components/SearchBox";
 
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <SearchBox/>
     </div>
   );
 }
