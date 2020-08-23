6a0ffb9b5c1b95219539c589c7c9aab7c0eef792
Sumnima B <sumnimabhandari@gmail.com>
2020-08-22 21:44:38 -0700

    Updates

---
+++ precache-manifest.442098adffe45840ab6567d62c8c6d9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fe233aa89452227d1ee962ac759ba67",
+    "url": "/react-employee-directory/index.html"
+  },
+  {
+    "revision": "41b7b6fcad7a78a0991a",
+    "url": "/react-employee-directory/static/css/main.6bc4a0cb.chunk.css"
+  },
+  {
+    "revision": "e86b2c88f1ada30f1255",
+    "url": "/react-employee-directory/static/js/2.f73d8caa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-employee-directory/static/js/2.f73d8caa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41b7b6fcad7a78a0991a",
+    "url": "/react-employee-directory/static/js/main.2dbf13f6.chunk.js"
+  },
+  {
+    "revision": "193cdcbd01f85fb428f8",
+    "url": "/react-employee-directory/static/js/runtime-main.76415422.js"
+  }
+]);
